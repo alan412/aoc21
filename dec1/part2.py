a3f8e5d01cc3857b921e6df6b3b66ec99c406a9b
Alan Smith <alan@randomsmiths.com>
2021-12-02 21:00:18 -0500

    minor cleanup

--- dec1/part2.py
+++ dec1/part2.py
@@ -18,11 +18,10 @@ def part2(data):
   numElements = len(data)
   oldData = data[0] + data[1] + data[2]
   numIncreased = 0
-  start = 0
   for i in range(numElements - 2):
     value = data[i] + data[i + 1] + data[i + 2]
     if value > oldData:
-      numIncreased = numIncreased + 1
+      numIncreased += 1
     oldData = value
 
   return numIncreased
