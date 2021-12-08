d23401352305132422c05a04a669488eb0ec002d
Alan Smith <alan@randomsmiths.com>
2021-12-08 11:15:26 -0500

    minor optimization

--- dec8/part2.py
+++ dec8/part2.py
@@ -15,62 +15,54 @@ class LEDs():
     self.signals = i.split()
     self.output = o.split()
     self.wiringMap = {}
-    self.digitMap = {}
     self.deduce()
 
   def deduce(self):
     unknown = []
+    digitMap = {}
     # Take care of easy ones first
     for digit in self.signals:
       wiring = frozenset(digit)
       if len(wiring) == 2:
-        self.wiringMap[wiring] = 1
-        self.digitMap[1] = wiring
+        digitMap[1] = wiring
       elif len(wiring) == 3:
-        self.wiringMap[wiring] = 7
-        self.digitMap[7] = wiring
+        digitMap[7] = wiring
       elif len(wiring) == 4:
-        self.wiringMap[wiring] = 4
-        self.digitMap[4] = wiring
+        digitMap[4] = wiring
       elif len(wiring) == 7:
-        self.wiringMap[wiring] = 8
-        self.digitMap[8] = wiring
+        digitMap[8] = wiring
       else:
         unknown.append(wiring)
 
     # We know that 3 is equal to 7 plus d and g
     for wiring in unknown:
-      if self.digitMap[7].issubset(wiring):
+      if digitMap[7].issubset(wiring):
         if len(wiring) == 5:
-          self.wiringMap[wiring] = 3
-          self.digitMap[3] = wiring
-        if len(wiring) == 6 and self.digitMap[4].issubset(wiring):
-          self.wiringMap[wiring] = 9
-          self.digitMap[9] = wiring
-    unknown.remove(self.digitMap[3])
-    unknown.remove(self.digitMap[9])
+          digitMap[3] = wiring
+        if len(wiring) == 6 and digitMap[4].issubset(wiring):
+          digitMap[9] = wiring
+    unknown.remove(digitMap[3])
+    unknown.remove(digitMap[9])
 
     # We know that 0 is equal to 1 plus digits
     # We know the other 6 wires left is 6
     for wiring in unknown:
       if len(wiring) == 6:
-        if self.digitMap[1].issubset(wiring):
-          self.wiringMap[wiring] = 0
-          self.digitMap[0] = wiring
+        if digitMap[1].issubset(wiring):
+          digitMap[0] = wiring
         else:
-          self.wiringMap[wiring] = 6
-          self.digitMap[6] = wiring
-    unknown.remove(self.digitMap[0])
-    unknown.remove(self.digitMap[6])
+          digitMap[6] = wiring
+    unknown.remove(digitMap[0])
+    unknown.remove(digitMap[6])
 
     # We know that 5 is a proper subset of 6
     for wiring in unknown:
-      if wiring.issubset(self.digitMap[6]):
-        self.wiringMap[wiring] = 5
-        self.digitMap[5] = wiring
+      if wiring.issubset(digitMap[6]):
+        digitMap[5] = wiring
       else:
-        self.wiringMap[wiring] = 2
-        self.digitMap[2] = wiring
+        digitMap[2] = wiring
+
+    self.wiringMap = {v: k for k, v in digitMap.items()}
 
   def getOutputValue(self):
     result = 0
